84a18ba34b6097a7a61bc015eef88f9c1c046be9
Stefan Ganser <ganser@fim.uni-passau.de>
2018-05-08 12:16:08 +0200

    Revert "hashode and equals for Generators"
    This reverts commit 6675e81dff6816f83dfe6e8db2d213c85e4b30f5.

--- src/org/exastencils/schedopt/chernikova/Generators.scala
+++ src/org/exastencils/schedopt/chernikova/Generators.scala
@@ -2,35 +2,36 @@ package org.exastencils.schedopt.chernikova
 
 import Chernikova.V
 
-case class Generators(hLines : Set[V], hRays : Set[V]) {
+case class Generators(hLines: Set[V], hRays: Set[V]) {
 
   // Initialize from generators in non-homogeneous coordinates,
   // i.e., lines, rays and vertices.
-  def this(nhLines : Set[V], nhRays : Set[V], nhVertices : Set[V]) =
-    this(nhLines.map(_ :+ (0 : BigInt)),
-      nhRays.map(_ :+ (0 : BigInt)) ++ nhVertices.map(_ :+ (1 : BigInt)))
+  def this(nhLines: Set[V], nhRays: Set[V], nhVertices: Set[V]) =
+    this(nhLines.map(_ :+ (0:BigInt)),
+         nhRays.map(_ :+ (0:BigInt)) ++ nhVertices.map(_ :+ (1:BigInt))
+        )
 
   // Lines in homogeneous coordinates
-  val homogeneousLines : Set[V] = hLines
+  val homogeneousLines: Set[V] = hLines
 
   // Rays in homogeneous coordinates
-  val homogeneousRays : Set[V] = hRays
+  val homogeneousRays: Set[V] = hRays
 
-  private def dropLast(v : V) : V = v.slice(0, v.length - 1)
+  private def dropLast(v: V): V = v.slice(0, v.length - 1)
 
   // Lines in ordinary coordinates
-  val lines : Set[V] = homogeneousLines map dropLast
+  val lines: Set[V] = homogeneousLines map dropLast
 
   // Rays in ordinary coordinates
-  val rays : Set[V] = homogeneousRays filter (_.last == 0) map dropLast
+  val rays: Set[V] = homogeneousRays filter (_.last == 0) map dropLast
 
   // Vertices in ordinary coordinates given as
   // (numerators of coordinates, common denominator) pairs
-  val vertices : Set[(V, BigInt)] =
-    homogeneousRays filter (_.last != 0) map ((r : V) => (dropLast(r), r.last))
+  val vertices: Set[(V, BigInt)] =
+    homogeneousRays filter (_.last != 0) map ((r: V) => (dropLast(r), r.last))
 
-  override def toString() : String = {
-    var sb : StringBuilder = new StringBuilder
+  override def toString(): String = {
+    var sb: StringBuilder = new StringBuilder
     sb =
       sb.append("Lines = ")
         .append(lines.map(_.mkString("(", ",", ")")).mkString("{ ", "; ", " }"))
@@ -39,31 +40,15 @@ case class Generators(hLines : Set[V], hRays : Set[V]) {
         .append(rays.map(_.mkString("(", ",", ")")).mkString("{ ", "; ", " }"))
         .append('\n')
 
-    def mkCoeff(n : BigInt, d : BigInt) : String = {
+    def mkCoeff(n: BigInt, d: BigInt): String = {
       if (d == 1)
         n.toString
       else
         n.toString + "/" + d.toString
     }
     sb = sb.append("Vertices = ")
-      .append(vertices.map((vd : (V, BigInt)) => vd._1.map(mkCoeff(_, vd._2)))
+      .append(vertices.map((vd: (V, BigInt)) => vd._1.map(mkCoeff(_, vd._2)))
         .map(_.mkString("(", ",", ")")).mkString("{ ", "; ", " }"))
     sb.toString()
-  }
-
-  override def equals(o : Any) : Boolean = {
-    if (o.isInstanceOf[Generators]) {
-      val oGenerators : Generators = o.asInstanceOf[Generators]
-      return oGenerators.lines.equals(lines) && oGenerators.rays.equals(rays) && oGenerators.vertices.equals(vertices)
-    }
-    return false
-  }
-
-  override def hashCode() : Int = {
-    val prime : Int = 13
-    var hashCode = lines.hashCode()
-    hashCode = hashCode * prime + rays.hashCode()
-    hashCode = hashCode * prime + vertices.hashCode()
-    return hashCode
   }
 }
